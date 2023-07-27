9c75df171381f6ed580235eb078a4e9af52bdd9e
Suraj Jha <134250774+Suraj-0828@users.noreply.github.com>
2023-07-27 10:29:29 +0530

    Add files via upload

---
+++ 10_relational_operators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	// relational operators
+	int num1 = 200;
+	int num2 = 400;
+	
+	// 0 = F
+	// 1 = T
+	
+	printf("%d \n",(num1 < num2));
+	printf("%d \n",(num1 > num2));
+	printf("%d \n",(num1 == num2));
+	printf("%d \n",(num1 != num2));
+	printf("%d \n",(num1 <= num2));
+	printf("%d \n",(num1 >= num2));
+}
---
+++ 7_valid and invalid statement.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	//this is valid statement
+	int a = 22;
+	int b = a;
+	int c = b + 10;
+	int d = 1, e;
+	// this is invalid statement
+//	int oldyear = 22;
+//	int newyear = oldyear + yearssss;
+//	int yearssss = 2;//this is wrong coding here bro
+//	return 0;
+}
